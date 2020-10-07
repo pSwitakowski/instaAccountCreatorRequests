27c46d9f2f3553313f5358d4bdda2e8cb7077159
Switakowski <piotr.switakowski@accenture.com>
2020-10-07 17:22:56 +0200

    selenium mail code verification

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-import requests
 import json
+import random
+import time
 
-headers1 = {
+import pyperclip
+from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+from proxies2 import proxies
+import requests
+import selenium
+
+def getDriver():
+
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("accept-language=en-US")  # set english browser language
+    options.add_argument(
+        "--disable-blink-features=AutomationControlled")  # disables "automated" pop-up, also helps not getting detected (or not), doesnt work hehe
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    driver = webdriver.Chrome(options=options, executable_path='C:\Python\chromedriver.exe')
+
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+        "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+    driver.execute_script("return navigator.userAgent;")
+    driver.delete_all_cookies()
+
+    return driver
+
+
+headers = {
     'Host': 'www.instagram.com',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
     'Accept': '*/*',
     'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
     'Accept-Encoding': 'gzip, deflate, br',
@@ -27,7 +63,7 @@ headers_code = {
     'Accept': '*/*',
     'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
     'Accept-Encoding': 'gzip, deflate, br',
-    # 'X-CSRFToken': 'gRUbRbU1rdQ4v8OPrn55Ofbrz1V6H1GL',
+    'X-CSRFToken': '',
     'X-Instagram-AJAX': '470fd86be390',
     'X-IG-App-ID': '936619743392459',
     'X-IG-WWW-Claim': '0',
@@ -36,7 +72,7 @@ headers_code = {
     'Origin': 'https://www.instagram.com',
     'Connection': 'keep-alive',
     'Referer': 'https://www.instagram.com/accounts/emailsignup/',
-    # 'Cookie': 'ig_did=9687644B-7EC1-45D2-858D-2F76BECA1A85; csrftoken=gRUbRbU1rdQ4v8OPrn55Ofbrz1V6H1GL; mid=X3y5sAALAAH7HmAjBAM4nO3exIUZ',
+    'Cookie': '',
     'TE': 'Trailers'
 }
 
@@ -64,11 +100,9 @@ def generate_csrf_token():
     return csrf_token, ig_did
 
 
-email = 'janusz.skowyr@o2.pl'
-full_name = 'janusz skowyr'
-username = 'janusz.skowyr.1234'
-password = 'owlcity1'
-
+full_name = 'janusz skowyr2'
+username = 'janusz.skowyr.222'
+# password = 'owlcity1'
 
 
 client_id = generateClientId()
@@ -79,15 +113,15 @@ print('csrf_token: ', csrf_token)
 print('ig_did: ', ig_did)
 
 
-headers1['X-CSRFToken'] = csrf_token
-headers1['Cookie'] = f'ig_cb=1; mid={client_id}; csrftoken={csrf_token}; ig_did={ig_did};'
+headers['X-CSRFToken'] = csrf_token
+headers['Cookie'] = f'ig_cb=1; mid={client_id}; csrftoken={csrf_token}; ig_did={ig_did};'
 
 headers_code['X-CSRFToken'] = csrf_token
 headers_code['Cookie'] = f'mid={client_id}; csrftoken={csrf_token}; ig_did={ig_did};'
 
 
 data = {
-    'email': email,
+    'email': '',
     'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1602024788:AXlQABuVy/rCsRztKdkOtDtS25flS2eH5HMggtqSnYI31gQ8Kl9Z3gQ3XUgxHnuIjqdtnBHOvDexmb2xmILEr2L87vOElD1cXEvBejMk9cFE91/HM0JP4X/vqAzdvwHUsTWszieoiLQKDRhJ',
     'username': username,
     'first_name': full_name.replace(' ', '+'),
@@ -96,36 +130,90 @@ data = {
     'opt_into_one_tap': 'false',
     'month': '7',
     'day': '12',
-    'year': '1990'
+    'year': '1990',
+    'tos_version': 'eu'
 }
 data_send_code = {
     'device_id': ig_did,
-    'email': email
+    'email': ''
 }
 data_verify_code = {
-    'code': '123456',
+    'code': '',
     'device_id': ig_did,
-    'email': email
+    'email': ''
 }
-
 
 cr_url = 'https://www.instagram.com/accounts/web_create_ajax/attempt/'
 code_url = 'https://i.instagram.com/api/v1/accounts/send_verify_email/'
 ver_url = 'https://i.instagram.com/api/v1/accounts/check_confirmation_code/'
 
+mail_url = 'https://temp-mail.org/pl/'
+driver = getDriver()
+random_mail = ''
+
+try:
+    driver.get(mail_url)
+    # inside the second tab
+    reset_button = WebDriverWait(driver, 8).until(
+        EC.presence_of_element_located((By.ID, 'click-to-delete')))
+    reset_button.click()
+
+    time.sleep(5)
+
+    # copy email to clipboard
+    ActionChains(driver).move_to_element(driver.find_element_by_id('click-to-copy')).click().perform()
+    random_mail = pyperclip.paste()
+except Exception as e:
+    print(e)
+    driver.quit()
+
+data['email'] = data_send_code['email'] = data_verify_code['email'] = random_mail
+
+print('random_mail: ', random_mail)
 
 with requests.session() as ses:
-    g = ses.get('https://www.instagram.com/accounts/emailsignup/')
+    # proxy_choice = random.choice(proxies)
+    # proxy = {"http": proxy_choice, "https": proxy_choice, 'ftp': proxy_choice}
+    # print("proxy:", proxy)
+    # # g = ses.get('https://www.instagram.com/accounts/emailsignup/')
 
-    p1 = ses.post(cr_url, headers=headers1, data=data)
+
+    p1 = ses.post(cr_url, headers=headers, data=data)
     print('p1 response code: ', p1)
     with open('response1.html', 'w') as f:
         f.write(p1.text)
+
+
+    # time.sleep(5)
 
     p_send_mail = ses.post(code_url, headers=headers_code, data=data_send_code)
     print('p_send_mail response code: ', p_send_mail)
     with open('response2.html', 'w') as f:
         f.write(p_send_mail.text)
+
+
+    #getting activation code from random mail
+
+    # wait for 2 minutes for mail to read activation code from it
+    WebDriverWait(driver, 120).until(
+        EC.presence_of_element_located(
+            (By.XPATH, '/html/body/main/div[1]/div/div[3]/div[2]/div/div[1]/div/div[4]/ul/li[2]/div[1]')))
+
+    # open mail
+    ActionChains(driver).move_to_element(driver.find_element_by_xpath(
+        '/html/body/main/div[1]/div/div[3]/div[2]/div/div[1]/div/div[4]/ul/li[2]/div[1]/a')).click().perform()
+
+    # get activation code from the mail
+    _mail_code = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH,
+                                        '//*[@id="email_content"]/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td[2]'))
+    ).text
+
+    data_verify_code['code'] = _mail_code
+
+    print("_mail_code:", _mail_code)
+
+    # driver.quit()
 
     headers_code['Content-Length'] = '126'
 
