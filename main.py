e510b0343bbb1a03d228a89a964b667172f8362a
pSwitakowski <p.switakowski1@gmail.com>
2020-10-07 02:02:13 +0200

    Working 2/3 requests

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 import requests
 import json
-import pprint
 
-headers = {
+headers1 = {
+    'Host': 'www.instagram.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0',
     'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
     'Accept-Encoding': 'gzip, deflate, br',
-    'X-Instagram-AJAX': 'cc6f59f85f33',
+    'X-CSRFToken': '',
+    'X-Instagram-AJAX': '470fd86be390',
     'X-IG-App-ID': '936619743392459',
     'X-IG-WWW-Claim': '0',
     'Content-Type': 'application/x-www-form-urlencoded',
     'X-Requested-With': 'XMLHttpRequest',
+    'Content-Length': '384',
     'Origin': 'https://www.instagram.com',
     'Connection': 'keep-alive',
     'Referer': 'https://www.instagram.com/accounts/emailsignup/',
-    'Pragma': 'no-cache',
-    'Cache-Control': 'no-cache',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+    'Cookie': '',
+    'TE': 'Trailers'
 }
-# csrftoken
-# ig_did
-# mid
-# limit = 1
-# name
-# email
-# username
-# proxy_file
+
+headers_code = {
+    'Host': 'i.instagram.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    'Accept': '*/*',
+    'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    # 'X-CSRFToken': 'gRUbRbU1rdQ4v8OPrn55Ofbrz1V6H1GL',
+    'X-Instagram-AJAX': '470fd86be390',
+    'X-IG-App-ID': '936619743392459',
+    'X-IG-WWW-Claim': '0',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': '61',
+    'Origin': 'https://www.instagram.com',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.instagram.com/accounts/emailsignup/',
+    # 'Cookie': 'ig_did=9687644B-7EC1-45D2-858D-2F76BECA1A85; csrftoken=gRUbRbU1rdQ4v8OPrn55Ofbrz1V6H1GL; mid=X3y5sAALAAH7HmAjBAM4nO3exIUZ',
+    'TE': 'Trailers'
+}
+
+headers_login_to_mail = {
+}
 
 
 def generateClientId():
@@ -35,29 +51,6 @@ def generateClientId():
         g = ses.get(url)
         return g.text
 
-# private function generateCsrfToken($proxy = null): bool
-# 	{
-# 		$strUrl = 'https://www.instagram.com/data/shared_data/?__a=1';
-#
-# 		$ch = curl_init();
-# 		curl_setopt($ch, CURLOPT_URL, $strUrl);
-# 		curl_setopt($ch, CURLOPT_USERAGENT, $this->user_agent);
-# 		if ($proxy)
-# 		{
-# 			curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-# 			curl_setopt($ch, CURLOPT_PROXY, $proxy);
-# 		}
-# 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-# 		$result = curl_exec($ch);
-# 		curl_close($ch);
-#
-# 		$json = json_decode($result, true);
-#
-# 		$this->csrftoken = $json['config']['csrf_token'];
-# 		$this->ig_did    = $json['device_id'];
-#
-# 		return true;
-# 	}
 
 def generate_csrf_token():
     url = 'https://www.instagram.com/data/shared_data/?__a=1'
@@ -71,23 +64,31 @@ def generate_csrf_token():
     return csrf_token, ig_did
 
 
-email = 'puste271@wp.pl'
+email = 'janusz.skowyr@o2.pl'
 full_name = 'janusz skowyr'
-username = 'janusz.skowyr.123'
-password = '123456!@#$%^'
+username = 'janusz.skowyr.1234'
+password = 'owlcity1'
 
 
 
 client_id = generateClientId()
 csrf_token, ig_did = generate_csrf_token()
 
-headers['X-CSRFToken'] = csrf_token
-headers['Cookie'] = f'ig_cb=1; mid={client_id}; csrftoken={csrf_token}; ig_did={ig_did}; rur=FRC'
+print('client_id (mid): ', client_id)
+print('csrf_token: ', csrf_token)
+print('ig_did: ', ig_did)
+
+
+headers1['X-CSRFToken'] = csrf_token
+headers1['Cookie'] = f'ig_cb=1; mid={client_id}; csrftoken={csrf_token}; ig_did={ig_did};'
+
+headers_code['X-CSRFToken'] = csrf_token
+headers_code['Cookie'] = f'mid={client_id}; csrftoken={csrf_token}; ig_did={ig_did};'
+
 
 data = {
     'email': email,
-    'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1601968787:AWRQAJGc3u2QxZavxESOD1qdtjFTxSh2ylt60Hv98r8X+2B4QOUJsNOm8wD0Auz6LzADM2OEVl68RhQY1wBuW9TX9Z8h9f+e209xX5Yd2HLaqzj7KPOaSshD8j9iS6sFFi8GthKr8Zsx0xRg',
-    'password': 'owlcity1',
+    'enc_password': '#PWD_INSTAGRAM_BROWSER:10:1602024788:AXlQABuVy/rCsRztKdkOtDtS25flS2eH5HMggtqSnYI31gQ8Kl9Z3gQ3XUgxHnuIjqdtnBHOvDexmb2xmILEr2L87vOElD1cXEvBejMk9cFE91/HM0JP4X/vqAzdvwHUsTWszieoiLQKDRhJ',
     'username': username,
     'first_name': full_name.replace(' ', '+'),
     'client_id': client_id,
@@ -97,17 +98,39 @@ data = {
     'day': '12',
     'year': '1990'
 }
+data_send_code = {
+    'device_id': ig_did,
+    'email': email
+}
+data_verify_code = {
+    'code': '123456',
+    'device_id': ig_did,
+    'email': email
+}
+
 
 cr_url = 'https://www.instagram.com/accounts/web_create_ajax/attempt/'
+code_url = 'https://i.instagram.com/api/v1/accounts/send_verify_email/'
+ver_url = 'https://i.instagram.com/api/v1/accounts/check_confirmation_code/'
+
+
 with requests.session() as ses:
-    p = ses.post(cr_url, headers=headers, data=data)
-    print(p)
-    with open('response.html', 'w') as f:
-        f.write(p.text)
+    g = ses.get('https://www.instagram.com/accounts/emailsignup/')
 
-# print('client id:', client_id)
-# print('csrf_token:', csrf_token)
-# print('ig_did:', ig_did)
+    p1 = ses.post(cr_url, headers=headers1, data=data)
+    print('p1 response code: ', p1)
+    with open('response1.html', 'w') as f:
+        f.write(p1.text)
 
+    p_send_mail = ses.post(code_url, headers=headers_code, data=data_send_code)
+    print('p_send_mail response code: ', p_send_mail)
+    with open('response2.html', 'w') as f:
+        f.write(p_send_mail.text)
 
+    headers_code['Content-Length'] = '126'
+
+    p_verify_code = ses.post(ver_url, headers=headers_code, data=data_verify_code)
+    print('p_verify_mail response code: ', p_verify_code)
+    with open('response3.html', 'w') as f:
+        f.write(p_verify_code.text)
 
---
+++ verify_mail.py
@@ -0,0 +1,20 @@
+import
+
+headers_mail = {
+    'Host': 'i.instagram.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0',
+    'Accept': '*/*',
+    'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    # 'X-CSRFToken': 'gRUbRbU1rdQ4v8OPrn55Ofbrz1V6H1GL',
+    'X-Instagram-AJAX': '470fd86be390',
+    'X-IG-App-ID': '936619743392459',
+    'X-IG-WWW-Claim': '0',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': '61',
+    'Origin': 'https://www.instagram.com',
+    'Connection': 'keep-alive',
+    'Referer': 'https://www.instagram.com/accounts/emailsignup/',
+    # 'Cookie': 'ig_did=9687644B-7EC1-45D2-858D-2F76BECA1A85; csrftoken=gRUbRbU1rdQ4v8OPrn55Ofbrz1V6H1GL; mid=X3y5sAALAAH7HmAjBAM4nO3exIUZ',
+    'TE': 'Trailers'
+}
